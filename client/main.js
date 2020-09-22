9cd5f4562854ecfc7e36e029acda16015cf4118a
Ademola Sobaki <demolasobaki@gmail.com>
2020-09-22 22:07:45 +0100

    fetch data from database

--- client/main.js
+++ client/main.js
@@ -1,22 +1,15 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Template } from 'meteor/templating'
+import { ReactiveVar } from 'meteor/reactive-var'
 
-import './main.html';
+import { Resolutions } from '../collections/resolution'
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+import './main.html'
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+// import Resolutions from '../collections/resolution'
+// const Resolutions = new Mongo.Collection('resolutions')
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Template.body.helpers({
+	resolutions: function () {
+		return Resolutions.find()
+	},
+})
