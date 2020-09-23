6f3ca68843b03a58fac764bf53be4b18a6cc3b22
Ademola Sobaki <demolasobaki@gmail.com>
2020-09-23 09:46:46 +0100

    add css styles

--- client/main.js
+++ client/main.js
@@ -5,11 +5,10 @@ import { Resolutions } from '../collections/resolution'
 
 import './main.html'
 
-// import Resolutions from '../collections/resolution'
-// const Resolutions = new Mongo.Collection('resolutions')
-
 Template.body.helpers({
 	resolutions: function () {
 		return Resolutions.find()
 	},
 })
+
+Template.body.events({})
